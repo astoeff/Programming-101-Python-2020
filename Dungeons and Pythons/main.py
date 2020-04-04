cfdcf1bf43bbc79a34fc07557718cbfad046126f
A.Stoev <antoni.1998@abv.bg>
2020-04-04 13:31:00 +0300

    Add changing screens for moving

--- Dungeons and Pythons/main.py
+++ Dungeons and Pythons/main.py
@@ -6,6 +6,7 @@ from help_library import get_character
 
 
 def print_intro():
+    new_screen()
     intro = 'Hi, a game has just started!\n\n' \
     'You are put in a magic dungeon with TREASURES (T) ' \
     'and your task is to make through the exit (G), but be carefull!\n' \
@@ -17,12 +18,9 @@ def print_intro():
 
 def print_legend():
     print('LEGEND:')
-    print('S - spawning position')
-    print('C - checkpoint')
-    print('G - exit of the dungeon')
-    print('T - treasure')
-    print('# - obstacle')
-    print('E - enemy')
+    print('S - spawning position           ' + 'C - checkpoint')
+    print('G - exit of the dungeon         ' + 'T - treasure')
+    print('# - obstacle                    ' + 'E - enemy')
 
 
 def print_dungeon_map(dungeon):
@@ -34,6 +32,7 @@ def print_map_with_legend(dungeon):
     print_dungeon_map(dungeon)
     print()
     print_legend()
+    print('\n')
 
 
 def new_screen():
@@ -41,12 +40,22 @@ def new_screen():
     subprocess.call(bashCommand)
 
 
-def wait_until_symbol_is_read_from_console(symbol, message):
+def wait_until_symbol_from_list_of_symbols_is_read_from_console(symbols, message):
     pressed = None
-    while str(pressed).lower() != symbol:
+    while True:
+        correct_symbol_read = False
+        for symbol in symbols:
+            if pressed == symbol:
+                correct_symbol_read = True
+                break
+        if correct_symbol_read is True:
+            return pressed
+            break
+        if pressed != None:
+            print(str(pressed))
         print(message)
         pressed = str(get_character())[2]
-        print(str(pressed))
+        pressed = pressed.lower()
 
 
 def create_hero():
@@ -84,7 +93,52 @@ def print_hero_current_state(hero):
             spaces += ' '
         print('*' + info[i] + spaces + '*')
     print(upper_border)
+    print()
+
+
+def print_navigation_legend():
+    print('NAVIGATION: w --> up\n'
+          '            s --> down\n'
+          '            a --> left\n'
+          '            d --> right')
     print('\n')
+
+
+def convert_symbol_pressed_to_direction(symbol_pressed):
+    if symbol_pressed == 'w':
+        return 'up'
+    elif symbol_pressed == 's':
+        return 'down'
+    elif symbol_pressed == 'a':
+        return 'left'
+    else:
+        return 'right'
+
+
+def validate_move_from_console(dungeon):
+    list_of_symbols = ['w', 's', 'a', 'd']
+    message = 'Press w,s,a or d to move ...'
+    correct = False
+    while correct is False:
+        symbol_pressed = wait_until_symbol_from_list_of_symbols_is_read_from_console(list_of_symbols, message)
+        direction = convert_symbol_pressed_to_direction(symbol_pressed)
+        if dungeon.move_hero(direction):
+            print('You moved your hero ', direction)
+            correct = True
+        else:
+            print('You cannot go {dir}, try different move\n'.format(dir=direction))
+
+
+def show_new_move_screen(dungeon, hero):
+    new_screen()
+    print_hero_current_state(hero)
+    print_navigation_legend()
+    print_map_with_legend(dungeon)
+    validate_move_from_console(dungeon)
+
+def moving_process(dungeon, hero):
+    while True:
+        show_new_move_screen(dungeon, hero)
 
 
 def main():
@@ -93,15 +147,16 @@ def main():
     print_map_with_legend(dungeon)
     symbol = 's'
     message = 'Press s for start'
-    wait_until_symbol_is_read_from_console(symbol, message)
+    wait_until_symbol_from_list_of_symbols_is_read_from_console([symbol], message)
     new_screen()
     hero = create_hero()
-    sleep(1)
-    new_screen()
-    dungeon.spawn(Hero)
-    print_hero_current_state(hero)
-    print_map_with_legend(dungeon)
-    sleep(10)
+    symbol = 'c'
+    message = 'Press c for continue ...'
+    wait_until_symbol_from_list_of_symbols_is_read_from_console([symbol], message)
+    # sleep(1)
+    dungeon.spawn(hero)
+    moving_process(dungeon, hero)
+    
     # while True:
     #     pass
 
