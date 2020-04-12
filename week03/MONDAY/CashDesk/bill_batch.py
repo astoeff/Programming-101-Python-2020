b3337efbee7bd5f15214f5eddf6722ddc85b7621
A.Stoev <antoni.1998@abv.bg>
2020-04-12 12:51:39 +0300

    Refactor cashdesk task code as PEP8 standart

--- week03/MONDAY/CashDesk/bill_batch.py
+++ week03/MONDAY/CashDesk/bill_batch.py
@@ -1,39 +1,39 @@
 from bill import Bill
 
+
 class BillBatch:
-	def __init__(self, batch):
-		if type(batch) != list:
-			raise TypeError('Invalid argument given, argument needs to be of type list!')
-		else:
-			for bill in batch:
-				if type(bill) != Bill:
-					raise TypeError('Invalid element of list given, argument needs to be list of elements of type Bill!')
-		self.batch = batch
+    def __init__(self, batch):
+        if type(batch) != list:
+            raise TypeError('Invalid argument given, argument needs to be of type list!')
+        else:
+            for bill in batch:
+                if type(bill) != Bill:
+                    raise TypeError('Invalid element of list given, argument needs to be list of elements of type Bill!')
+        self.batch = batch
 
-	def __len__(self):
-		return len(self.batch)
+    def __len__(self):
+        return len(self.batch)
 
-	def __getitem__(self, index):
-		if index in range(len(self)):
-			return self.batch[index]
-		else:
-			raise IndexError('Index out of boundary!')
+    def __getitem__(self, index):
+        if index in range(len(self)):
+            return self.batch[index]
+        else:
+            raise IndexError('Index out of boundary!')
 
-	def total(self):
-		return sum(self.batch)
-
+    def total(self):
+        return sum(self.batch)
 
 
 if __name__ == '__main__':
-	values = [10, 20, 50, 100]
-	bills = [Bill(value) for value in values]
+    values = [10, 20, 50, 100]
+    bills = [Bill(value) for value in values]
 
-	batch = BillBatch(bills)
+    batch = BillBatch(bills)
 
-	for bill in batch:
-		print(bill)
+    for bill in batch:
+        print(bill)
 
-	print(sum(batch))
+    print(sum(batch))
 
 # A 10$ bill
 # A 20$ bill
--- week03/MONDAY/CashDesk/cashdesk.py
+++ week03/MONDAY/CashDesk/cashdesk.py
@@ -1,58 +1,59 @@
 from bill import Bill
 from bill_batch import BillBatch
 
+
 class Cashdesk:
-	def __init__(self):
-		self.money_table = {}
+    def __init__(self):
+        self.money_table = {}
 
-	def __take_bill(self, bill):
-		if bill in self.money_table:
-				self.money_table[bill] += 1
-		else:
-				self.money_table[bill] = 1
+    def __take_bill(self, bill):
+        if bill in self.money_table:
+                self.money_table[bill] += 1
+        else:
+                self.money_table[bill] = 1
 
-	def take_money(self, money):
-		if type(money) != Bill and type(money) != BillBatch:
-			raise TypeError('Invalid argument given, argument needs to be instance of Bill or BillBatch')
-		if type(money) == Bill:
-			self.__take_bill(money)
-		else:
-			for bill in money.batch:
-				self.__take_bill(bill)
+    def take_money(self, money):
+        if type(money) != Bill and type(money) != BillBatch:
+            raise TypeError('Invalid argument given, argument needs to be instance of Bill or BillBatch')
+        if type(money) == Bill:
+            self.__take_bill(money)
+        else:
+            for bill in money.batch:
+                self.__take_bill(bill)
 
-	def total(self):
-		return sum(bill.amount * self.money_table[bill] for bill in self.money_table.keys())
+    def total(self):
+        return sum(bill.amount * self.money_table[bill] for bill in self.money_table.keys())
 
-	def __repr__(self):
-		representation = ''
-		list_of_money_in_table = [i for i in self.money_table]
-		sorted_list = sorted(list_of_money_in_table, key = lambda c:c.amount)
-		for el in sorted_list:
-			representation += str(str(el)[2:]) + 's - ' + str(self.money_table[el]) + '\n'
-		return representation	
+    def __repr__(self):
+        representation = ''
+        list_of_money_in_table = [i for i in self.money_table]
+        sorted_list = sorted(list_of_money_in_table, key=lambda c: c.amount)
+        for el in sorted_list:
+            representation += str(str(el)[2:]) + 's - ' + str(self.money_table[el]) + '\n'
+        return representation
 
-	def inspect(self):
-		print(self.__repr__())	
-		# return self.__repr__()   #used for unit testing
+    def inspect(self):
+        print(self.__repr__())
+        # return self.__repr__()   #used for unit testing
 
 
 if __name__ == '__main__':
-	cashdesk = Cashdesk()
+    cashdesk = Cashdesk()
 
-	cashdesk.take_money(Bill(10))
-	batch = BillBatch([Bill(i) for i in range(1,5)])
-	cashdesk.take_money(batch)
-	cashdesk.inspect()
+    cashdesk.take_money(Bill(10))
+    batch = BillBatch([Bill(i) for i in range(1, 5)])
+    cashdesk.take_money(batch)
+    cashdesk.inspect()
 
-	values = [10, 20, 50, 100, 100, 100]
-	bills = [Bill(value) for value in values]
+    values = [10, 20, 50, 100, 100, 100]
+    bills = [Bill(value) for value in values]
 
-	batch = BillBatch(bills)
+    batch = BillBatch(bills)
 
-	desk = Cashdesk()
+    desk = Cashdesk()
 
-	desk.take_money(batch)
-	desk.take_money(Bill(10))
+    desk.take_money(batch)
+    desk.take_money(Bill(10))
 
-	print(desk.total()) # 390
-	desk.inspect()
+    print(desk.total())  # 390
+    desk.inspect()
