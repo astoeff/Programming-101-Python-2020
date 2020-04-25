ed9c7d0e94edda2b0d29b11d9698b521dd42982c
A.Stoev <antoni.1998@abv.bg>
2020-04-25 15:38:24 +0300

    Print like animation

--- week08/WEDNESDAY/Frogs/OOP_SOLUTION/oop_solution.py
+++ week08/WEDNESDAY/Frogs/OOP_SOLUTION/oop_solution.py
@@ -1,4 +1,6 @@
 from pond import Pond
+import subprocess
+import time
 
 
 if __name__ == '__main__':
@@ -6,9 +8,13 @@ if __name__ == '__main__':
     pond = Pond(frogs_pers_side)
     pond.set_pond_string()
     print(pond)
+    time.sleep(1)
+    subprocess.call('clear')
     while not pond.is_arranged():
         pond.move_frog()
         print(pond)
+        time.sleep(1)
+        subprocess.call('clear')
 
 
 print('\nResult:', pond.pond_string)
