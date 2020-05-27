9174fc4e8f385f9d53ed5ea308326a3dc577fcc2
A.Stoev <antoni.1998@abv.bg>
2020-05-27 14:56:36 +0300

    Add initial structure of project with templates

--- Project/mysite/polls/urls.py
+++ Project/mysite/polls/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/questions/', views.QuestionDetailView.as_view(), name='q_detail'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    
     # path('', views.home, name='home'),
-    path('<int:pk/', views.QuestionsView.as_view(), name='questions')
+    # path('<int:category_price/', views.QuestionsView.as_view(), name='questions')
 ]
--- Project/mysite/polls/views.py
+++ Project/mysite/polls/views.py
@@ -30,58 +30,70 @@ class IndexView(generic.ListView):
         return Category.objects.all().order_by('price')
 
 
-class QuestionsView(generic.ListView):
+# class QuestionsView(generic.ListView):
+#     model = Category
+#     template_name = 'polls/questions.html'
+#     # context_object_name = 'latest_question_list'
+
+#     # def get_queryset(self):
+#     #     """
+#     #     Return the last five published questions (not including those set to be
+#     #     published in the future).
+#     #     """
+#     #     return Category.objects.all()
+
+
+class DetailView(generic.DetailView):
     model = Category
-    template_name = 'polls/questions.html'
-    # context_object_name = 'latest_question_list'
+    template_name = 'polls/detail.html'
+
+    # def get_queryset(self):
+    #     """
+    #     Excludes any questions that aren't published yet.
+    #     """
+    #     return Category.objects.filter(price__lte=500)
+
+
+class QuestionDetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/q_detail.html'
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
-        published in the future).
+        Excludes any questions that aren't published yet.
         """
-        return Category.objects.all()
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-# class DetailView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/detail.html'
 
-#     def get_queryset(self):
-#         """
-#         Excludes any questions that aren't published yet.
-#         """
-#         return Question.objects.filter(pub_date__lte=timezone.now())
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
-
-# class ResultsView(generic.DetailView):
-#     model = Question
-#     template_name = 'polls/results.html'
-
-#     def get_queryset(self):
-#         """
-#         Excludes any questions that aren't published yet.
-#         """
-#         return Question.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 # def home(request):
